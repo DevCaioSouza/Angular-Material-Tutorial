49238ec0723c93bfe37baaa67bdcd9b68afe768d
unknown <caiocbahia@hotmail.com>
2022-12-02 19:08:38 -0300

    button toggle + button toggle group

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 const MaterialComponents = [
   MatButtonModule,
   MatCardModule,
   MatFormFieldModule,
-  MatSelectModule
+  MatSelectModule,
+  MatButtonToggleModule
 ]
 
 @NgModule({
