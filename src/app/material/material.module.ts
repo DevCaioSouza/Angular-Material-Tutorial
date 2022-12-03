15309e019bccc724170d38a7c36de79f6febcf6b
unknown <caiocbahia@hotmail.com>
2022-12-02 22:18:12 -0300

    icons class

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,7 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 
 const MaterialComponents = [
@@ -11,7 +12,8 @@ const MaterialComponents = [
   MatCardModule,
   MatFormFieldModule,
   MatSelectModule,
-  MatButtonToggleModule
+  MatButtonToggleModule,
+  MatIconModule
 ]
 
 @NgModule({
